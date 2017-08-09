9e58083e6113d4da25790fcb16795ecf24ff5c52
Artem Suschev <Suschev.Artem@gmail.com>
2017-08-09 18:56:27 +0300

    Update route-perf.js

--- addon/mixins/route-perf.js
+++ addon/mixins/route-perf.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   perfTracking: Ember.inject.service('performance-tracking'),
-  _actions: {
+  actions: {
     /**
      * Schedule a function to call the endTransition function of the perfTracking service in afterRender run queue
      */
