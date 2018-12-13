cdb403bf698760c711b5b0019afd960a7d6a9327
nagits <aleksey.nagits@gmail.com>
2018-12-13 18:52:58 +0500

    Reopen router instead of routes to ensure hooks will trigger properly

--- addon/mixins/route-perf.js
+++ addon/mixins/route-perf.js
@@ -2,39 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   perfTracking: Ember.inject.service('performance-tracking'),
-  actions: {
-    /**
-     * Schedule a function to call the endTransition function of the perfTracking service in afterRender run queue
-     */
-    didTransition: function() {
-      var self = this;
-      Ember.run.scheduleOnce('afterRender', function () {
-        self.get('perfTracking').endTransition(self.routeName, self.get('router.url'));
-      });
-      const result = this._super.apply(this, arguments);
-      if (typeof result === "undefined") {
-        return true;
-      } else {
-        return result;
-      }
-    },
-    willTransition: function() {
-      var originRoute = this.get('routeName');
-      this.get('perfTracking').startTransition(originRoute);
-      const result = this._super.apply(this, arguments);
-      // result may be true, false, some Object, undefined
-      // result is undefined if:
-      // 1. no handler for willTransition in current route
-      // 2. handler intentionally return undefined
-      //
-      // in case 1 we should return true instead of undefined
-      // and send event to next route
-      // in case 2 we should change handler
-      if (typeof result === "undefined") {
-        return true;
-      } else {
-        return result;
-      }
-    }
+
+  didTransition() {
+    this._super(...arguments);
+
+    // Ensure `url` has been updated.
+    Ember.run.next(this, function() {
+      this.get('perfTracking').endTransition(
+        this.get('currentRouteName'),
+        this.get('url')
+      );
+    });
+  },
+
+  willTransition() {
+    var originRoute = this.get('routeName');
+    this.get('perfTracking').startTransition(originRoute);
+
+    this._super(...arguments);
   }
 });
